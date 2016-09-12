9dd8291096f81d2922869aac87184952f133eb00
monsteronfire <monsteronfire@gmail.com>
2016-09-13 00:11:50 +0800

    Add post and user models

---
+++ app/models/post.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+import Model from 'ember-data/model';
+
+const {
+  attr
+} = DS;
+
+export default Model.extend({
+  title: attr('string'),
+  body: attr('string')
+});
