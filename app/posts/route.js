db5c050f1c5f8142a3b42b373843befc7d1d2f73
monsteronfire <monsteronfire@gmail.com>
2016-09-13 23:51:52 +0800

    Use setupController for posts

--- app/posts/route.js
+++ app/posts/route.js
@@ -1,7 +1,21 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const {
+  Route,
+  set
+} = Ember;
+
+export default Route.extend({
   model() {
     return this.store.findAll('post');
+  },
+
+  //I don't want to keep having to call our model "model"
+  //because it is too generic and will get super confusing
+  //when we have lots of other types of data
+  //so we set the name below:
+  setupController(controller, model) {
+    //set property on my controller, want to call it 'posts' and bind it to model
+    set(controller, 'posts', model);
   }
 });
