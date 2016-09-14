4ab4ed1c71f80f729af2b90b8f8f724e0c1c0660
monsteronfire <monsteronfire@gmail.com>
2016-09-14 22:54:55 +0800

    Add index route - template and source js
    * Also moved code from parent route level to index sub-route level

--- app/posts/route.js
+++ app/posts/route.js
@@ -6,16 +6,4 @@ const {
 } = Ember;
 
 export default Route.extend({
-  model() {
-    return this.store.findAll('post');
-  },
-
-  //I don't want to keep having to call our model "model"
-  //because it is too generic and will get super confusing
-  //when we have lots of other types of data
-  //so we set the name below:
-  setupController(controller, model) {
-    //set property on my controller, want to call it 'posts' and bind it to model
-    set(controller, 'posts', model);
-  }
 });
