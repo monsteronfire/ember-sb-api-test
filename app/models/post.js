d9ee921c9918a6f5d6f1fce4bce8abac847d7d28
monsteronfire <monsteronfire@gmail.com>
2016-09-14 12:52:54 +0800

    Change userId key to user

--- app/models/post.js
+++ app/models/post.js
@@ -7,7 +7,7 @@ const {
 } = DS;
 
 export default Model.extend({
-  userId: belongsTo('user'),
+  user: belongsTo('user'),
 
   title: attr('string'),
   body: attr('string')
--- app/serializers/post.js
+++ app/serializers/post.js
@@ -4,6 +4,14 @@ export default DS.RESTSerializer.extend({
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
     //console.log(payload);
     payload = { posts: payload };
+
+    //userId isn't as clear or straightforward as "user"
+    //So we're going to map the userId to user below
+    //Change every key that is "userId" and update it to "user"
+    payload.posts.forEach((post) => {
+      post.user = post.userId;
+      delete post.userId;
+    });
     //Because we're overriding this method, we want to make sure we don't completely override it
     //return this._super(..arguments);
     //We can't actually use '..arguments' because arguments would be referrencing the original payload
